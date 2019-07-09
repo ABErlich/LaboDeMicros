c71222cd782a6ea163a74a0ee6a36f7fd9798156
Axel Erlich <axeluck1@gmail.com>
2019-07-09 17:57:43 -0300

    commit

--- Led/Led/Led.asm
+++ Led/Led/Led.asm
@@ -17,25 +17,16 @@ main:
 
 		sbi	DDRB,7 // Configuro el puerto B7 COMO SALIDA
 loop:
-		cbi PORTB, 7
+		sbi PORTB, 7
 		rcall demora
-		sbi	PORTB, 7 
+		cbi	PORTB, 7 
 		rcall demora
 		rjmp loop
 
 demora:
 		ldi r16, 255
-loop2:
-		ldi r17, 255
-loop3:
-		ldi r18, 5
-loop4:
-		dec r18
-		brne loop4
-		dec r17
-		brne loop3
 		dec r16
-		brne loop2
+		brne demora
 		
 		ret
 
